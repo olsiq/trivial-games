98cf779481d9525d56a6ff101e76d99c384a6a20
olsiq <64708189+olsiq@users.noreply.github.com>
2022-06-08 19:24:27 +0300

    adding slider

--- src/components/games/poker/controls/Controls.jsx
+++ src/components/games/poker/controls/Controls.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Button } from "../../../button";
+import { Slider } from "../../../slider";
 export const Controls = () => {
   return (
     <div className="controls">
       <Button action={"raise"} content={"raise"} />
       <Button action={"fold"} content={"fold"} />
       <Button action={"check"} content={"check"} />
+      <Slider />
     </div>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ import { GameLayout, BetsLayout, ControlsLayout } from "./layout";
 import { Game } from "./games";
 import { Card } from "./cards/Card";
 import { Header } from "./header";
-export { GameLayout, Game, BetsLayout, ControlsLayout, Card, Header };
+import { Slider } from "./slider";
+export { GameLayout, Game, BetsLayout, ControlsLayout, Card, Header, Slider };
