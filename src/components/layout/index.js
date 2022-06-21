85e63e289693eb22b6bb882f289a2c120cea096e
olsiq <64708189+olsiq@users.noreply.github.com>
2022-06-21 16:55:40 +0300

    rmv uselesss comps

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,6 +1,5 @@
 import { GameLayout } from "./game/GameLayout";
-// import { ControlsLayout } from "./controls/ControlsLayout";
-// import { BetsLayout } from "./bets/BetsLayout";
+
 import { MineSweeperLayout } from "./minesweeper/MineSweeperLayout";
 
 export { GameLayout, MineSweeperLayout };
